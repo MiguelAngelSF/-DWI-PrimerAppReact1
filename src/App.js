7810df66bc9729896f9c7bb80a12c63c45b27a17
DII UTAGS <miguelangel.sf@outlook.com>
2022-06-12 18:04:01 -0500

    feat:New functionality is added -Structure of functions and first mathematical operation

--- src/App.js
+++ src/App.js
@@ -14,8 +14,26 @@ function App() {
     },
   ]);
 
+  const [operation, setOperation] = useState([
+    {
+      result: "",
+      Number1: "",
+      Number2: ""
+    },
+  ])
+
   const [name, setName] = useState("");
   const [lastName, setLastname] = useState("");
+  
+
+  const [Number1, setNumber1] = useState("");
+  const [Number2, setNumber2] = useState("");
+  const [result, setResult] = useState("");
+
+  const sum = function (e) {
+    setResult(Number(Number1) + Number(Number2));
+  };
+    
 
   return (
     <div className="App">
@@ -45,6 +63,54 @@ function App() {
           <li id={idx}>{`${person.name} ${person.lastName}`}</li>
         ))}
       </ul>
+      
+      <label for="Number1">Ingresa el Primer Numero:
+      </label>
+      <input
+        type="number"
+        name="Number1"
+        onChange={(event) => {
+          setNumber1(event.target.value);
+        }}
+      />
+      <br/>
+      <br/>
+      <label for="Number1">Ingresa el Segundo Numero:
+      </label>
+      <input
+        type="number"
+        name="Number2"
+        onChange={(event) => {
+          setNumber2(event.target.value);
+        }}
+      />
+      <br/>
+      {/* <button
+        onClick={()=> {
+          setOperation((current) => [{ Number1, Number2, result }, ...current]);
+        }}
+      >
+        Add user
+      </button> */}
+      <button className="btn" onClick={sum}>Sumar</button>
+      <br/>
+      <br/>
+      <label for="Number1">Resultado:
+      </label>
+      <input
+        type="text"
+        name="results"
+        value={result}
+        disabled
+      />
+      {/* <ul>
+        {operation.map((operation, idx) => (
+          <li id={idx}>{`${operation.result}`}</li>
+        ))}
+      </ul> */}
+    
+      
+
     </div>
   );
 }
